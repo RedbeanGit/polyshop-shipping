4925fe95cf037866615b6133dd49790f609fd488
RedbeanGit <dubois.julien.mail@gmail.com>
2023-05-05 16:43:32 +0200

    fix: remove eureka
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/shipping/ShippingApplication.java
+++ src/main/java/fr/dopolytech/polyshop/shipping/ShippingApplication.java
@@ -2,10 +2,8 @@ package fr.dopolytech.polyshop.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ShippingApplication {
 
 	public static void main(String[] args) {
